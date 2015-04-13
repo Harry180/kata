489af58c75cdcabd77a9b286ec4f73a35884c64a
harry <lukasz.charubin1989@gmail.com>
2015-04-13 22:24:59 +0200

    Initial commit.First test phase red.

---
+++ Calculator/Calculator.Tests/CalculatorAddServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Calculator.Tests
+{
+    public class CalculatorAddServiceTests
+    {
+        [Fact]
+        public void should_return_zero_when_value_as_a_empty_string()
+        {
+            const string value = "";
+
+            //act
+            var result = new CalculatorLib.CalculatorAddService().Add(value);
+
+            //assert
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ Calculator/CalculatorLib/CalculatorAddService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorLib
+{
+    public class CalculatorAddService
+    {
+        public int Add(string value)
+        {
+            throw new Exception();
+        }
+    }
+}
