f602f9c7a052b73a94fe956775b6bb48001e6807
harry <lukasz.charubin1989@gmail.com>
2015-04-13 22:43:21 +0200

    2 extra tests red.

--- Calculator/Calculator.Tests/CalculatorAddServiceTests.cs
+++ Calculator/Calculator.Tests/CalculatorAddServiceTests.cs
@@ -4,16 +4,45 @@ namespace Calculator.Tests
 {
     public class CalculatorAddServiceTests
     {
+        private CalculatorLib.CalculatorAddService _calculatorAddService = new CalculatorLib.CalculatorAddService();
+
         [Fact]
         public void should_return_zero_when_value_as_a_empty_string()
         {
             const string value = "";
 
             //act
-            var result = new CalculatorLib.CalculatorAddService().Add(value);
+            var result = _calculatorAddService.Add(value);
 
             //assert
             Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void should_return_same_value_than_passed()
+        {
+            const string value = "1";
+
+            //act
+            var result = _calculatorAddService.Add(value);
+
+            //assert
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void should_return_sum_of_values_passed()
+        {
+            const string value = "1,2";
+            const int expectedValue = 3;
+
+            //act
+            var result = _calculatorAddService.Add(value);
+
+            //assert
+
+            Assert.Equal(expectedValue, result);
         }
     }
 }
